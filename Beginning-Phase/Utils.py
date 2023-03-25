1f920495d1104d715eb194792743cb2d3120a057
LukeGorgadze <97985323+LukeGorgadze@users.noreply.github.com>
2023-03-25 23:36:43 +0400

    Up

--- Beginning-Phase/Utils.py
+++ Beginning-Phase/Utils.py
@@ -1,6 +1,8 @@
 import numpy as np
+import cv2 as cv
+import matplotlib.pyplot as plt
 
-def transform2Dto1D(mat):
+def from2Dto1DHorizontal(mat):
     rows, cols = mat.shape
     res = []
     for row in range(rows):
@@ -9,6 +11,14 @@ def transform2Dto1D(mat):
 
     return np.array(res)
 
+def from2Dto1DVertical(mat):
+    rows, cols = mat.shape
+    res = []
+    for col in range(cols):
+        for row in range(rows):
+            res.append(mat[row][col])
+
+    return np.array(res)
 
 
 def listArrayToString(narray):
@@ -17,3 +27,76 @@ def listArrayToString(narray):
         res += str(narray[i]) + "|"
     res = res[:len(res)-1]
     return res
+
+
+def TransformImagesToData(MODE, amountLimiter, emnist_images, emnist_labels,horizontal):
+    """Transforms 2D images into 1D, writes them into text files and returns list of such arrays"""
+    path = ""
+    resList = []
+    if MODE == "letters":
+        path = "Beginning-Phase/Letters.txt"
+    elif MODE == "digits":
+        path = "Beginning-Phase/Digits.txt"
+    with open(path, 'w') as file:
+        amount = amountLimiter if amountLimiter < emnist_images.shape[
+            0] else emnist_images.shape[0]
+        for i in range(amount):
+            im = emnist_images[i]
+            if MODE == "letters":
+                # label for characters
+                imLabel = chr(emnist_labels[i] + ord('a') - 1)
+            elif MODE == "digits":
+                # label for numbers
+                imLabel = str(emnist_labels[i])
+
+            newMat = None
+
+            if horizontal:
+                newMat = from2Dto1DHorizontal(im)
+            else:
+                newMat = from2Dto1DVertical(im)
+            resList.append(newMat)
+
+            newMatTxt = listArrayToString(newMat)
+            res = imLabel + "-" + newMatTxt + '\n'
+            file.write(res)
+    return resList
+
+
+def showImages(MODE, amountLimiter, emnist_images, emnist_labels, width, height,data):
+    """Shows images and prompts out what symbol this picture corresponds to in terminal"""
+    for i in range(amountLimiter):
+        im = emnist_images[i]
+        img = cv.resize(im, (width, height))
+        if MODE == "letters":
+            # print for letters
+            print(chr(emnist_labels[i] + ord('a') - 1))
+        elif MODE == "digits":
+            # print for digits
+            print(emnist_labels[i])
+
+        # Display the image with text
+        cv.imshow('Image and Text', img)
+        plotGraphs(data[i])
+
+        # Wait for a key press and then close the window
+        cv.waitKey(0)
+        k = cv.waitKey(0)
+        if k == ord('s') or k == ord('S'):  # "S" or "s" key
+            break
+
+def plotGraphs(arrData):
+    Xs = [x for x in range(len(arrData))]
+    Ys = [y for y in arrData]
+    # Create a line plot
+    plt.plot(Xs, Ys)
+    # plt.scatter(Xs, Ys, s=10, marker='o')
+
+    # Add title and axis labels
+    plt.title("Line Graph")
+    plt.xlabel("X-axis label")
+    plt.ylabel("Y-axis label")
+
+    # Show the plot
+    plt.show()
+   
--- Beginning-Phase/main.py
+++ Beginning-Phase/main.py
@@ -1,13 +1,13 @@
 import numpy as np
 import emnist
 import cv2 as cv
-from Utils import listArrayToString
+from Utils import TransformImagesToData,showImages
 
+MODE = 'letters'
 # Load the EMNIST dataset
-emnist_data = emnist.extract_training_samples('letters')
+emnist_data = emnist.extract_training_samples(MODE)
 
 # Separate the images and labels
-# print(emnist_data[2])
 emnist_images = emnist_data[0]
 emnist_labels = emnist_data[1]
 
@@ -20,45 +20,18 @@ width, heigth = emnist_images[0].shape
 
 # For scaling up the image
 scale_factor = 20
-amountLimiter = 40000
+amountLimiter = 100
 
 newWidth = int(scale_factor * width)
 newHeight = int(scale_factor * heigth)
 
 
-def writeIntoFile():
-    with open('Beginning-Phase/Digits.txt', 'w') as file:
-        amount = amountLimiter if amountLimiter < emnist_images.shape[
-            0] else emnist_images.shape[0]
-        for i in range(amount):
-            im = emnist_images[i]
-            # label for characters
-            imLabel = chr(emnist_labels[i] + ord('a') - 1)
-            # label for numbers
-            # imLabel = str(emnist_labels[i])
-            newMat = im.flatten()
-            newMatTxt = listArrayToString(newMat)
-            res = imLabel + "-" + newMatTxt + '\n'
-            file.write(res)
-# writeIntoFile()
+data = TransformImagesToData(MODE,amountLimiter,emnist_images,emnist_labels,False)
+showImages(MODE,amountLimiter,emnist_images,emnist_labels,newWidth,newHeight,data)
 
 
-for i in range(1000):
-    im = emnist_images[i]
-    img = cv.resize(im, (newWidth,newHeight))
-    # print for letters
-    print(chr(emnist_labels[i] + ord('a') - 1))
-    # print for digits
-    # print(emnist_labels[i])
 
-    # Display the image with text
-    cv.imshow('Image and Text', img)
 
-    # Wait for a key press and then close the window
-    cv.waitKey(0)
-    k = cv.waitKey(0)
-    if k == ord('s') or k == ord('S'):  # "S" or "s" key
-        break
 
 
 cv.destroyAllWindows()
